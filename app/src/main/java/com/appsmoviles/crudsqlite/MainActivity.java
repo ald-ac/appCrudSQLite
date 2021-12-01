4b2a9e803be8afcb15e8e427dffb70752f480d37
ald-ac <muriasaldair@hotmail.com>
2021-12-01 00:09:26 -0600

    Boton para limpiar campos funcional

--- app/src/main/java/com/appsmoviles/crudsqlite/MainActivity.java
+++ app/src/main/java/com/appsmoviles/crudsqlite/MainActivity.java
@@ -136,6 +136,14 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+        //Accion limpiar campos
+        ivLimpiar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                limpiarCampos();
+            }
+        });
     }
 
     //LIMPIAR EDITTEXT
